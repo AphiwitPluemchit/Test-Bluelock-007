329412b2c33f1a82628ce22f56f6c2b434a0752c
aphiwitrr <168804537+aphiwitrr@users.noreply.github.com>
2025-08-27 02:09:06 +0700

    Enhance login test in Cypress and update environment variable handling

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL || "http://localhost:9000",
+    baseUrl: process.env.VITE_APP_URL || "http://localhost:9000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    env: {
+      VITE_APP_URL: process.env.VITE_APP_URL || "http://localhost:9000",
     },
   },
 });
--- cypress/e2e/Login/ex.cy.js
+++ cypress/e2e/Login/ex.cy.js
@@ -1,5 +1,25 @@
-describe("example to-do app", () => {
-  it("passes", () => {
-    expect(true).to.equal(true);
-  });
-});
+describe('login', () => {
+  it('passes', () => {
+    // บังคับเป็น desktop (กันสลับไป mobile)
+
+    cy.visit('/')
+
+    // Email
+    cy.get('[data-testid="login-email"]')
+      .filter(':visible')        // กันเลือกตัวที่ถูกซ่อน
+      .scrollIntoView()
+      .click()
+      .type('65160205@go.buu.ac.th')
+
+    // Password
+    cy.get('[data-testid="login-password"]')
+      .filter(':visible')
+      .scrollIntoView()
+      .click({ force: true }) 
+      .type('123456')
+
+    cy.get('[data-testid="login-submit"]')
+      .filter(':visible')
+      .click()
+  })
+})
