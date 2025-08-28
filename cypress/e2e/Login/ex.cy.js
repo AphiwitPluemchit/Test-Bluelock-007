80508660ff884135cc1bf870b46825e070bf5b24
aphiwitrr <168804537+aphiwitrr@users.noreply.github.com>
2025-08-28 21:42:45 +0700

    Update login test selectors for mobile compatibility and add project setup instructions

--- cypress/e2e/Login/ex.cy.js
+++ cypress/e2e/Login/ex.cy.js
@@ -2,23 +2,24 @@ describe('login', () => {
   it('passes', () => {
     // บังคับเป็น desktop (กันสลับไป mobile)
 
+
     cy.visit('/')
 
     // Email
-    cy.get('[data-testid="login-email"]')
+    cy.get('[data-testid="mobile-login-email"]')
       .filter(':visible')        // กันเลือกตัวที่ถูกซ่อน
       .scrollIntoView()
       .click()
       .type('65160205@go.buu.ac.th')
 
     // Password
-    cy.get('[data-testid="login-password"]')
+    cy.get('[data-testid="mobile-login-password"]')
       .filter(':visible')
       .scrollIntoView()
       .click({ force: true }) 
       .type('123456')
 
-    cy.get('[data-testid="login-submit"]')
+    cy.get('[data-testid="mobile-login-submit"]')
       .filter(':visible')
       .click()
   })
